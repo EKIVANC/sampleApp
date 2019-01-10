597cb6cd87cfd4fbd6322a52940dac7aa9f2819a
ekivanc <ekivanc@deloitte.com>
2019-01-11 00:15:50 +0300

    chmod +x ./before_install.sh

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 source /home/ec2-user/.bash_profile
+chmod +x ./before_install.sh
 ./before_install.sh
 cd /var/app
 echo $PWD
--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 source /home/ec2-user/.bash_profile
+chmod +x ./before_install.sh
 ./before_install.sh
 forever stopall

086c3e9d696a26c8b93891445c25a54b69dd9c48
ekivanc <ekivanc@deloitte.com>
2019-01-10 21:56:10 +0300

    install node

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 source /home/ec2-user/.bash_profile
-# add nodejs to yum
-curl --silent --location https://rpm.nodesource.com/setup_10.x | bash -
-yum -y install nodejs
+curl -o- https://raw.github.com/creationix/nvm/v0.32.0/install.sh | bash
+. ~/.nvm/nvm.sh
+nvm install 10.13.0
 node -e "console.log('Running Node.js ' + process.version)"
 echo 'installing forever'
 npm i forever -g

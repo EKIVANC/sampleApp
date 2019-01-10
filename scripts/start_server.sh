4ca6d649996ed0f945acd36f5ecc28dbffaccaa5
ekivanc <ekivanc@deloitte.com>
2019-01-11 00:23:11 +0300

    - location: scripts/before_install.sh

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 source /home/ec2-user/.bash_profile
-chmod +x ./before_install.sh
-./before_install.sh
 cd /var/app
 echo $PWD
 forever start app.js
--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 source /home/ec2-user/.bash_profile
-chmod +x ./before_install.sh
-./before_install.sh
 forever stopall

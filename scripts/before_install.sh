a8202c82e9b95a76eb3b70b8e976091a065c3b33
ekivanc <ekivanc@deloitte.com>
2019-01-11 13:55:05 +0300

    update URL to githubusercontent

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 source /home/ec2-user/.bash_profile
-curl -o- https://raw.github.com/creationix/nvm/v0.32.0/install.sh | bash
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.32.0/install.sh | bash
 . ~/.nvm/nvm.sh
 nvm install 10.13.0
 node -e "console.log('Running Node.js ' + process.version)"
